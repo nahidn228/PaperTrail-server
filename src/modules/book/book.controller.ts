e60a5783692eb47ca48162689a645a5ce18fe4cd
nahidn228 <nahidn228@gmail.com>
2025-09-04 21:45:46 +0600

    update book controller

--- src/modules/book/book.controller.ts
+++ src/modules/book/book.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import Book from "./book.model";
 import { SortOrder } from "mongoose";
+import { sendResponse } from "../../utils/SendResponse";
+import status from "http-status";
 
 const createBook = async (req: Request, res: Response) => {
   try {
@@ -21,97 +23,70 @@ const createBook = async (req: Request, res: Response) => {
 };
 
 const getAllBook = async (req: Request, res: Response) => {
-  try {
-    // const {
-    //   filter,
-    //   sortBy = "createdAt",
-    //   sort = "asc",
-    //   limit = "10",
-    // } = req.query;
+  const {
+    filter,
+    sortBy = "createdAt",
+    sort = "asc",
+    limit = "10",
+  } = req.query;
 
-    // const filterCondition: Record<string, unknown> = {};
-    // if (filterCondition) {
-    //   filterCondition.genre = filter?.toString().toUpperCase();
-    // }
-
-    // const sortCondition: Record<string, SortOrder> = {};
-    // sortCondition[sortBy.toString()] = sort === "desc" ? -1 : 1;
-
-    // const data = await Book.find(filterCondition)
-    //   .sort(sortCondition)
-    //   .limit(Number(limit));
-
-    const data = await Book.find();
-
-    res.send({
-      success: true,
-      message: "Books retrieved successfully",
-      data,
-    });
-  } catch (error) {
-    res.status(500).send({
-      success: false,
-      message: "Books retrieved Unsuccessful",
-      error,
-    });
+  const filterCondition: Record<string, unknown> = {};
+  if (filterCondition) {
+    filterCondition.genre = filter?.toString().toUpperCase();
   }
+
+  const sortCondition: Record<string, SortOrder> = {};
+  sortCondition[sortBy.toString()] = sort === "desc" ? -1 : 1;
+
+  const data = await Book.find(filterCondition)
+    .sort(sortCondition)
+    .limit(Number(limit));
+
+  // const data = await Book.find();
+
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: "Books retrieved successfully",
+    data: data,
+  });
 };
 
 const getBookById = async (req: Request, res: Response) => {
-  try {
-    const bookId = req.params.bookId;
-    const data = await Book.findById(bookId);
-    res.send({
-      success: true,
-      message: "Book retrieved successfully",
-      data,
-    });
-  } catch (error) {
-    res.status(500).send({
-      success: false,
-      message: "Book retrieved Unsuccessful",
-      error,
-    });
-  }
+  const bookId = req.params.bookId;
+  const data = await Book.findById(bookId);
+
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: "Books retrieved successfully",
+    data: data,
+  });
 };
 
 const updateBook = async (req: Request, res: Response) => {
-  try {
-    const bookId = req.params.bookId;
-    const data = await Book.findByIdAndUpdate(bookId, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.send({
-      success: true,
-      message: "Book update successfully",
-      data,
-    });
-  } catch (error) {
-    res.status(500).send({
-      success: false,
-      message: "Book update Unsuccessful",
-      error,
-    });
-  }
+  const bookId = req.params.bookId;
+  const data = await Book.findByIdAndUpdate(bookId, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: "Book update successfully",
+    data: data,
+  });
 };
 const deleteBook = async (req: Request, res: Response) => {
-  try {
-    const bookId = req.params.bookId;
-    const data = await Book.findByIdAndDelete(bookId);
-
-    res.send({
-      success: true,
-      message: "Book deleted successfully",
-      data: null,
-    });
-  } catch (error) {
-    res.status(500).send({
-      success: false,
-      message: "Book deletion unsuccessful",
-      error,
-    });
-  }
+  const bookId = req.params.bookId;
+  const data = await Book.findByIdAndDelete(bookId);
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: "Book deleted successfully",
+    data: null,
+  });
 };
 
 export const bookController = {
